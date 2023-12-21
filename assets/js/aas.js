69628ef97de503a74f04e4744fef8f16c55438a8
adikgh <adiikgh@gmail.com>
2023-12-21 17:25:11 +0600

    1.002

---
+++ assets/js/aas.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+
+
+
+
+   // $('.lz_o7').lazy({effect:"fadeIn",effectTime:500,threshold:0})
+
+   // var blo7_Swiper = new Swiper(".blo7_Swiper", {
+   //    effect: "creative",
+   //    speed: 600,
+   //    resistanceRatio: 0,
+   //    grabCursor: !0,
+   //    parallax: !0,
+   //    creativeEffect: {
+   //       limitProgress: 3,
+   //       perspective: !0,
+   //       shadowPerProgress: !0,
+   //       prev: {
+   //          shadow: !0,
+   //          translate: ["-15%", 0, -200]
+   //       },
+   //       next: {
+   //          translate: ["calc(100% + 20px)", 0, 0]
+   //       }
+   //    },
+   //    on:{slideChange:function(){$('.lz_o7').lazy({effect:"fadeIn",effectTime:500,threshold:0})}},
+   //    pagination: {
+   //       el: ".o7_pagination",
+   //       dynamicBullets: true,
+   //    },
+   // });
+
+
+
+
+
+})
+
+
+
+   const player_o7 = new Plyr(".player_o7",{
+      fullscreen: {iosNative: true},
+      controls: ['play-large'],
+      poster: '/assets/img/result/chrome_0oD9KqL9vH.jpg',
+   });
+   const player_o8 = new Plyr(".player_o8",{
+      fullscreen: {iosNative: true},
+      controls: ['play-large'],
+      poster: '/assets/img/result/chrome_0oD9KqL9vH.jpg',
+   });
+
+   const player_o9 = new Plyr(".player_o9",{
+      fullscreen: {iosNative: true},
+      controls: ['play-large'],
+      poster: '/assets/img/result/chrome_0oD9KqL9vH.jpg',
+   });
+   const player_o10 = new Plyr(".player_o10",{
+      fullscreen: {iosNative: true},
+      controls: ['play-large'],
+      poster: '/assets/img/result/chrome_0oD9KqL9vH.jpg',
+   });
+   const player_o11 = new Plyr(".player_o11",{
+      fullscreen: {iosNative: true},
+      controls: ['play-large'],
+      poster: '/assets/img/result/chrome_0oD9KqL9vH.jpg',
+   });
+
---
+++ autotrading/index.php
@@ -0,0 +1,234 @@
+<? include "../config/core.php";
+
+   
+	$site_name = 'autotrading';
+   $site_set['header'] = false;
+   $site_set['swiper'] = true;
+   $site_set['plyr'] = true;
+   $site_set['footer'] = false;
+   $css = ['autotrading'];
+   $js = ['aas'];
+
+
+
+   // $whatsapp = 'https://wa.me/77750055123';
+   // $wh_txt1 = 'Хочу отправить чек ..';
+   // $wh_txt2 = 'Хотелось оплатить на рассрочку ..';
+?>
+<? include "../block/header.php"; ?>
+<? unset($_SESSION['loader']); ?>
+
+
+   <div class="lbl1">
+      <div class="lbl1_t">
+         <div class="lazy_img" data-src="/assets/img/bag/sl_022321_41020_29.jpg"></div>
+      </div>
+      <div class="bl_c">
+
+         <div class="lbl1_c">
+            <div class="lbl1_cm">
+               <h1>АвтоТрейдинг арқылы <br> <span>айына 20%-ға дейін</span> <br> тұрақты табыс тап</h1>
+               <div class="btn">Тегін кеңес алу</div>
+            </div>
+            <div class="lbl1_cr">
+               <div class="lbl1_crs lazy_img" data-src="/assets/img/bag/IMG_4701 ы.png"></div>
+               <div class="lbl1_cmc">
+                  <div class="lbl1_cmh">Әлібек Бауыржанұлы</div>
+                  <div class="lbl1_cmp">2014 жылдан бастап финансовый <br> рынокта торговля жасаймын</div>
+               </div>
+            </div>
+         </div>
+         
+         <div class="scroll-downs">
+            <div class="mousey">
+               <div class="scroller"></div>
+            </div>
+         </div>
+      </div>
+   </div>
+
+
+   <div class="lbl2">
+      <div class="bl_c">
+         <div class="head_c">
+            <h4>Менің мақсатым, әр АвтоТрейдинг қолданушысы депазитін <br> қауіпсіздікте сақтай отыра тұрақты табыс табуын үйрету</h4>
+         </div>
+         <div class="lbl2_c">
+            <div class="lbl2_l">
+               <div class="lbl2_lp">Менің компаниям <span>инвестициялық компания емес</span>, ешқандай ақшаны басқаруға алмайды.</div>
+               <div class="lbl2_lp">АвтоТрейдинг-тің табатын әрбір табысы <span>Forex нарығында валюталық жұптарды алу және сату</span> операциясы арқылы жүзеге асады.</div>
+               <div class="lbl2_lb">
+                  <div class="lbl2_lbs lazy_img" data-src="/assets/img/bag/443_finance-removebg-preview.png"></div>
+                  <div class="lbl2_lbd"><span>Қатаң түрде ескертеміз.</span> Биржа нарығында немесе кез-келген қаржы нарығында <span>өзіңіздің жеке қаржыңызбен жұмыс жасаңыз.</span> Себебі табыс бар жерде тәуекелде бар.</div>
+               </div>
+            </div>
+            <div class="lbl2_r">
+               <!-- <div class="lbl2_rs">Біз жайлы білгіңіз келсе видеоны қараңыз</div> -->
+               <div class="lbl2_rd">
+                  <div class="player_o7" data-plyr-provider="youtube" data-plyr-embed-id="xx2TOKid4SA"></div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+
+
+   <div class="lbl3">
+      <div class="bl_c">
+         <div class="lbl3_c">
+            <div class="lbl3_r">
+               <!-- <div class="">Роботтың жұмыс жасау тәртібі</div> -->
+               <div class="lbl3_rd">
+                  <div class="player_o8" data-plyr-provider="youtube" data-plyr-embed-id="xx2TOKid4SA"></div>
+               </div>
+            </div>
+            <div class="lbl3_l">
+               <div class="head_c">
+                  <h4>АвтоТрейдинг алгоритмі Forex нарығында қалай жұмыс жасайды?</h4>
+               </div>
+               <div class="lbl3_lp">Әлемдік қаржы нарығында роботтарды қолдану фондтар мен трейдерлерді өте үлкен өзгерістер мен жетістіктерге алып келеді</div>
+               <div class="btn">Толық ақпарат алу</div>
+            </div>
+         </div>
+      </div>
+   </div>
+
+
+   <div class="lbl4">
+      <div class="bl_c">
+         <div class="head_c">
+            <h4 class="txt_c">АвтоТрейдинг ұтымды, жылдам әрі ыңғайлы</h4>
+         </div>
+         <div class="lbl4_с">
+            <div class="lbl4_i">
+               <!-- <div class="">01</div> -->
+               <div class="lbl4_ih">Трейдингте тәжірибеңіз болуы міндетті емес</div>
+               <div class="lbl4_ip">Алгоритмді орнату мен баптауларына арнап толық <span>видео-нұсқаулықпен қамтамасыз етеміз.</span></div>
+               <div class="lbl4_im">
+                  <div class="lazy_img" data-src="/assets/img/bag/pensive-customer-thinking-over-special-offer-removebg-preview.png"></div>
+               </div>
+            </div>
+            <div class="lbl4_i">
+               <!-- <div class="">02</div> -->
+               <div class="lbl4_ih">Толық автоматтандырылған</div>
+               <div class="lbl4_ip">Робот 24 сағат бойы сіздің қатысуыңызсыз волюталық жұпты сатып алып, сату опрацисын жасайды. <span>Айына 5% бен 25% аралығында</span> табыс табады.</div>
+               <div class="lbl4_im">
+                  <div class="lazy_img" data-src="/assets/img/bag/robot-relaxing-on-a-laptop-removebg-preview.png"></div>
+               </div>
+            </div>
+            <div class="lbl4_i">
+               <!-- <div class="">04</div> -->
+               <div class="lbl4_ih">Уақытыңызды бағалайды</div>
+               <div class="lbl4_ip">Робот күні бойы уақыт бөлуді қажет етпейді. Кез-келген уақытта телефоныңыздан қарап отыра аласыз.</div>
+               <div class="lbl4_im">
+                  <div class="lazy_img" data-src="/assets/img/bag/gold-coin-in-the-hourglass-time-is-money-concept-removebg-preview.png"></div>
+               </div>
+            </div>
+            <div class="lbl4_i">
+               <!-- <div class="">03</div> -->
+               <div class="lbl4_ih">Эмоцияға берілмейді</div>
+               <div class="lbl4_ip">Роботтың адамнан айырмашылығы 24 сағат бойы эмоцияға берілмей, жазып қойған алгаритм бойынша табыс табады.</div>
+               <div class="lbl4_im">
+                  <div class="lazy_img" data-src="/assets/img/bag/robot-science-technology-automation-innovation-removebg-preview.png"></div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+
+
+   <div class="lbl5">
+      <div class="bl_c">
+         <div class="head_c txt_c">
+            <h4>АвтоТрейдинг қолданушылардың шынайы пікірлері</h4>
+            <p>Біздің роботты пайдалану барысында көңілі толмаған клиент болған жоқ</p>
+         </div>
+         <div class="lbl5_c">
+            <div class="lbl5_cs">
+               <div class="player_o9" data-plyr-provider="youtube" data-plyr-embed-id="xx2TOKid4SA"></div>
+            </div>
+            <div class="lbl5_cs">
+               <div class="player_o10" data-plyr-provider="youtube" data-plyr-embed-id="xx2TOKid4SA"></div>
+            </div>
+            <div class="lbl5_cs">
+               <div class="player_o11" data-plyr-provider="youtube" data-plyr-embed-id="xx2TOKid4SA"></div>
+            </div>
+         </div>
+         <!-- <div class="">
+            <div class="btn">Тегін кеңес алу</div>
+         </div> -->
+      </div>
+   </div>
+
+
+   <div class="lbl6">
+      <div class="bl_c">
+         <div class="head_c">
+            <h4 class="txt_c">Біз жұмыс жасайтын брокер</h4>
+         </div>
+         <div class="lbl6_c">
+            <div class="lbl6_r">
+               <div class="lazy_img" data-src="/assets/img/bag/exness.png"></div>
+            </div>
+            <div class="lbl6_l">
+               <div class="">Exness - Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis eaque nobis reiciendis explicabo esse quas rem ipsum, hic maxime qui dolore ea facilis totam vero quaerat cum labore amet veritatis. Hic maxime qui dolore ea facilis totam vero quaerat cum labore amet veritatis.</div>
+            </div>
+         </div>
+      </div>
+   </div>
+
+
+   <div class="lbl7">
+      <div class="bl_c">
+         <div class="lbl7_a">
+            <div class="lazy_img" data-src="/assets/img/bag/silver-coin-stacks-money-currency-finance-savings-investment-concept-background-3d-illustration-removebg-preview.png"></div>
+         </div>
+         <div class="lbl7_c">
+            <div class="head_c lbl7_l">
+               <h4>7 күнге АвтоТрейдинг алгоритмін тест режимінде тегін қолданыңыз</h4>
+            </div>
+            <div class="lbl7_r">
+               <div class="btn">Тегін пайдалану</div>
+            </div>
+         </div>
+      </div>
+   </div>
+
+
+   <footer class="footer">
+      <div class="bl_c">
+
+         <div class="footer_t">
+            <div class="footer_tt">
+               <div class="footer_ttl">
+                  <div class="footer_tth"><?=$site['name']?></div>
+                  <div class="footer_ttc">
+                     <div class="footer_ttp">Біз Қазақстанда 2019 жылдан бастап жұмыс жасаймыз. Қазіргі таңда бізбен жұмыс жасап, тұрақты табыс тауып отырған 3000-нан астам қолданушы бар.</div>
+                  </div>
+               </div>
+               <div class="footer_ttr">
+                  <div class="footer_ttri">
+                     <div class="footer_tth">Сайт саясаты</div>
+                     <div class="footer_ttc">
+                        <a href="/about/privacy-security/privacy-policy.php">Құпиялылық саясаты</a>
+                        <a href="/about/privacy-security/terms-conditions.php">Ережелер мен шарттар</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+
+         <div class="footer_b">
+            <div class="footer_bl">© <?=$site['name']?>, 2023</div>
+            <div class="footer_br">
+               <a href="https://gprog.kz" target="_blank" class="gprog_bl">
+                  <span>#gprog-та</span>
+                  <div class="gprog_heart"><i class="fas fa-heart"></i></div>
+                  <span>жасалған</span>
+               </a>
+            </div>
+         </div>
+      </div>
+   </footer>
+
+<? include "../block/footer.php"; ?>
